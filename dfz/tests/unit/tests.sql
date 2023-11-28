26ac0eeaa5bdcf06f24cab7f0f11a43311020c1c
Sai Krishna Reddy <tonysai6995@gmail.com>
2023-11-28 16:36:23 +0530

    commited

--- dfz/tests/unit/tests.sql
+++ dfz/tests/unit/tests.sql
@@ -4,22 +4,25 @@
     )
 }}
 
-
-{% call dbt_unit_testing.test('customers', 'should show customer_id without orders') %}
+{% call dbt_unit_testing.test('customers', 'should sum order values to calculate customer_lifetime_value') %}
   
   {% call dbt_unit_testing.mock_ref ('stg_customers') %}
     select 1 as customer_id, '' as first_name, '' as last_name
   {% endcall %}
-
+  
   {% call dbt_unit_testing.mock_ref ('stg_orders') %}
-    select null::numeric as customer_id, null::numeric as order_id, null as order_date  where false
-  {% endcall %}
-
-  {% call dbt_unit_testing.mock_ref ('stg_payments') %}
-     select null::numeric as order_id, null::numeric as amount where false
+    select 1001 as order_id, 1 as customer_id, null as order_date
+    UNION ALL
+    select 1002 as order_id, 1 as customer_id, null as order_date
   {% endcall %}
   
+  {% call dbt_unit_testing.mock_ref ('stg_payments') %}
+    select 1001 as order_id, 10 as amount
+    UNION ALL
+    select 1002 as order_id, 20 as amount
+  {% endcall %}
+
   {% call dbt_unit_testing.expect() %}
-    select 1 as customer_id
+    select 1 as customer_id, 30 as customer_lifetime_value
   {% endcall %}
 {% endcall %}
